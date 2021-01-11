e0beb45cef069fb64e943ca9ca9676565cc85ee4
Omar <ramo_24@outlook.com>
2021-01-11 15:10:53 +0200

    main menu

--- src/main.java
+++ src/main.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) throws IOException {
+        while(true)
+            menu();
+
+    }
+
+    static void menu(){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Choose 1 to compress a file, 2 to decompress a file: ");
         System.out.println("1. Compress a file: ");
         System.out.println("2. Decompress a file: ");
+        System.out.println("3. exit ");
         int choice = scanner.nextInt();
         if (choice == 1) {
-            FileHandler.fileReader();
-        } else {
-            BuildHuffmanTree.DecompressFile();
+            try {
+                FileHandler.fileReader();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (choice == 2) {
+            try {
+                BuildHuffmanTree.DecompressFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (choice == 3){
+            System.exit(0);
         }
-
+        else
+            System.err.println("not recognized input, try again");
     }
 }
